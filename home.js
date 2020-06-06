7ec0795dcdb258d5b62576993b8b17068dcb0463
Moymon <47367207+Moymon@users.noreply.github.com>
2020-06-05 22:07:37 -0500

    GET productos

--- home.js
+++ home.js
@@ -9,7 +9,7 @@ botonBuscar.addEventListener('click', buscar);
 
 //var pro="-1";
 
-//let productos =[];
+let productos =[];
 
 /*Funcion que carga los productos al home de la página*/
 function cargaProductos(e) {
@@ -19,28 +19,19 @@ function cargaProductos(e) {
 
     var xhr = new XMLHttpRequest();
     //xhr.open('GET', "productos.json", true);
-    xhr.open("GET", "http://localhost:80/Gaby's%20shop/productos", true);
+    xhr.open("GET", "./Controllers/productoController.php", true);
     //xhr.setRequestHeader("Content-Type", "application/json");
-    
-    console.log("entre 1");
 
-    
     //console.log(this.responseText);
     xhr.onload = function () {//Funcion que lee lo que hay en el JSON para llenar la lista
         if (this.status === 200) {
-            console.log("entre 2");
             console.log(this.status);
-            //console.log(this.responseText);
+            console.log(this.responseText);
             var data = JSON.parse(this.responseText);
-            //console.log(data);
-            
             if (data.success === true){
-                console.log("entre 3");
-                let productos = data.data.productos;
-                console.log(productos);
-                //var html = "";
-                productos.forEach(producto => {
-                    var html = "";
+                productos = data.data;
+                var html = "";
+                productos.productos.forEach(producto => {
                     html += `
                         <div class="Productos col-m-3 col-s-12 p-r-1" id="producto">
                             <div class="prod border col-m-12 col-s-12">
@@ -48,8 +39,9 @@ function cargaProductos(e) {
                                     
                                 </div>
                                 <div class="b-prod-top-s col-m-12 col-s-6">
+                                    <img src="${producto.imagen}">    
                                     <div class="m-1">${producto.nombre}</div>
-                                    <div class="m-1">Precio: $ ${producto.precio}</div>
+                                    <div class="m-1">Precio: $ ${producto.precio} </div>                
                                 </div>
                             </div>
                         </div> `;
