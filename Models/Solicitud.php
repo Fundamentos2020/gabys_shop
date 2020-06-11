98b3b2e7495babf753867a357b9499b09dfa51ad
Moymon <47367207+Moymon@users.noreply.github.com>
2020-06-11 07:01:46 -0500

    modificacion estructura

--- Models/Solicitud.php
+++ Models/Solicitud.php
@@ -4,29 +4,29 @@
 
     class Solicitud{
 
-        private $_id;
+        private $_id_solicitud;
         private $_id_vendedor;
-        private $_archivo;
+        private $_solicitudRuta;
         private $_id_admi;
         private $_aprobada;
 
-        public function __construct($id, $id_vendedor, $archivo, $id_admi, $aprobada){
-            $this->setId($id);
+        public function __construct($id_solicitud, $id_vendedor, $solicitudRuta, $id_admi, $aprobada){
+            $this->setId($id_solicitud);
             $this->setIdVendedor($id_vendedor);
-            $this->setArchivo($archivo);
+            $this->setSolicitudR($solicitudRuta);
             $this->setIdAdmi($id_admi);
             $this->setAprobada($aprobada);
        }
 
         public function getId(){
-            return $this->_id;
+            return $this->_id_solicitud;
         }
 
         public function setId($id){
-            if($id !== null && (!is_numeric($id) || $id <=0 || $id >= 2147483647) || $this->_id !== null){
+            if($id !== null && (!is_numeric($id) || $id <=0 || $id >= 2147483647) || $this->_id_solicitud !== null){
                 throw new SolicitudException("Error en ID de solicitud");
             }
-            $this->_id = $id;
+            $this->_id_solicitud = $id;
         }
 
         public function getIdVendedor(){
@@ -40,23 +40,12 @@
             $this->_id_vendedor = $id_vendedor;
         }
 
-        public function getArchivo(){
-            return $this->_archivo;
-        }
-
-        public function setArchivo($archivo){
-            if($archivo === null){
-                throw new SolicitudException("Error en archivo, Solicitud");
-            }
-            $this->_archivo = $archivo;
-        }
-
         public function getIdAdmi(){
             return $this->_id_admi;
         }
 
         public function setIdAdmi($id_admi){
-            if(!is_numeric($id_admi) || $id_admi <=0 || $id_admi >= 2147483647){
+            if(!is_numeric($id_admi) /*|| /*$id_admi >=0*/ || $id_admi >= 2147483647){
                 throw new SolicitudException("Error en ID de admi, Solicitud");
             }
             $this->_id_admi = $id_admi;
@@ -67,18 +56,28 @@
         }
 
         public function setAprobada($aprobada){
-            if(/*$aprobado !== true ||*/ $aprobado === null /*|| $aprobado !== false*/ ){
+            if(/*$aprobado !== true ||*/ $aprobada === null /*|| $aprobado !== false*/ ){
                 throw new SolicitudException("Error en aprobada de solicitud");
             }
             $this->_aprobada = $aprobada;
         }
 
+        public function getSolicitudR(){
+            return $this->_solicitudRuta;
+        }
+
+        public function setSolicitudR($aprobada){
+            if(/*$aprobado !== true ||*/ $aprobada === null /*|| $aprobado !== false*/ ){
+                throw new SolicitudException("Error en aprobada de solicitud");
+            }
+            $this->_solicitudRuta = $aprobada;
+        }
 
         public function getSolicitud(){
             $solicitud = array();
             $solicitud['id_solicitud'] = $this->getId();
             $solicitud['id_vendedor'] = $this->getIdVendedor();
-            $solicitud['archivo'] = $this->getArchivo();
+            $solicitud['solicitudRuta'] = $this->getSolicitudR();
             $solicitud['id_admi'] = $this->getIdAdmi();
             $solicitud['aprobada'] = $this->getAprobada();
             return $solicitud;
