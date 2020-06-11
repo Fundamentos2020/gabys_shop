e23c7c8e81460e822bb5dcae0d5caa52a8b7d714
AlfredoOrtiz14 <40581292+AlfredoOrtiz14@users.noreply.github.com>
2020-06-11 13:12:20 -0500

    Checa tokens de acceso

--- Controllers/solicitudController.php
+++ Controllers/solicitudController.php
@@ -18,7 +18,71 @@
         exit();
     }
 
-    //Checar sesiones
+    if (!isset($_SERVER['HTTP_AUTHORIZATION']) || strlen($_SERVER['HTTP_AUTHORIZATION']) < 1) {
+        $response = new Response();
+        $response->setHttpStatusCode(401);
+        $response->setSuccess(false);
+        $response->addMessage("No se encontró el token de acceso");
+        $response->send();
+        exit();
+    }
+
+    $accesstoken = $_SERVER['HTTP_AUTHORIZATION']; 
+
+
+    try {
+        $query = $connection->prepare('SELECT caducidad_token_acceso FROM sesiones WHERE token_acceso = :token_acceso');
+        $query->bindParam(':token_acceso', $accesstoken, PDO::PARAM_STR);
+        $query->execute();
+    
+        $rowCount = $query->rowCount();
+    
+        if ($rowCount === 0) {
+            $response = new Response();
+            $response->setHttpStatusCode(401);
+            $response->setSuccess(false);
+            $response->addMessage("Token de acceso no válido");
+            $response->send();
+            exit();
+        }
+    
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+    
+        //$consulta_idUsuario = $row['id_usuario'];
+        $consulta_cadTokenAcceso = $row['caducidad_token_acceso'];
+        //$consulta_activo = $row['activo'];
+    
+        /*if($consulta_activo !== 'SI') {
+            $response = new Response();
+            $response->setHttpStatusCode(401);
+            $response->setSuccess(false);
+            $response->addMessage("Cuenta de usuario no activa");
+            $response->send();
+            exit();
+        }*/
+        date_default_timezone_set("America/Mexico_City");
+    
+        if (strtotime($consulta_cadTokenAcceso) < time()) {
+            $response = new Response();
+            $response->setHttpStatusCode(401);
+            $response->setSuccess(false);
+            $response->addMessage("Token de acceso ha caducado");
+            $response->send();
+            exit();
+        }
+    } 
+    catch (PDOException $e) {
+        error_log('Error en DB - ' . $e);
+    
+        $response = new Response();
+        $response->setHttpStatusCode(500);
+        $response->setSuccess(false);
+        $response->addMessage("Error al autenticar usuario");
+        $response->send();
+        exit();
+    }
+
+
     if(array_key_exists('aprobado',$_GET)){
         $aprobado = $_GET['aprobado'];
         if ($aprobado == '' || !is_numeric($aprobado)) {
